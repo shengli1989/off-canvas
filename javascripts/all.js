e6b760d896b4d43b90b6382e98f00411dfb139fa
shengli1989 <shengli@evendesign.tw>
2014-02-07 21:20:24 +0800

    like code drop menu

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,45 +1,33 @@
 var off_canvas = function (){
 
   var body_scroll_top;
-  var outside_inner_scroll_top;
-
 
   // 展開 menu
   $(".open-menu-btn").on("click", function(event){
     // 記憶滾動數值
+
     body_scroll_top = $('body').scrollTop(); // body
-    outside_inner_scroll_top = $('.outside-inner').css('top').replace("px",""); //從字串中取出數值
 
     $("body").addClass("open-menu");
-
-
     // 繼承滾動數值
     $('.page').on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
       $('body').addClass('scroll-control');
-      $('.page').scrollTop(body_scroll_top);
-      $('.outside-inner').scrollTop(-outside_inner_scroll_top).css('top', 0);
+      $('.page').css('top', body_scroll_top * -1 +'px');
     });
-
-    event.preventDefault();
 
   });
 
 
   // 關閉 menu
-  $(".outside a, .off-canvas-mask").on("click", function(){
+  $(".off-canvas-mask").on("click", function(){
 
     // 記憶滾動數值
-    outside_inner_scroll_top = $('.outside-inner').scrollTop();
-    body_scroll_top = $('.page').scrollTop();
-
     $("body").removeClass("open-menu");
-
-
-  // 繼承滾動值
-  $('.page').on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
-    $("body").removeClass("scroll-control").scrollTop(body_scroll_top);
-    $('.outside-inner').css('top', -outside_inner_scroll_top);
-  });
+    // 繼承滾動值
+    $('.page').on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
+      $("body").removeClass("scroll-control").scrollTop(body_scroll_top);
+      $('.page').css('top', 0);
+    });
 
   });
 
